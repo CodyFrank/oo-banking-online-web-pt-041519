b4cc59eaadad53f41e4821b1cf0b8bc1fc7e0393
Cody Frank <cody.frank30@gmail.com>
2019-05-26 13:53:11 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -17,7 +17,7 @@ class Transfer
   end
 
   def execute_transaction
-    if valid?
+    if valid? && self.status == "pending"
       @sender.balance -= @amount
       @receiver.balance += @amount
       self.status = "complete"
