cbced50ed6c2d7e8907ce47cb9906bc7b32f55ca
Cody Frank <cody.frank30@gmail.com>
2019-05-26 14:03:46 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -30,5 +30,10 @@ class Transfer
   end
 
   def reverse_transfer
+    if @status == "complete"
+      @receiver.balance -= @amount
+      @sender.balance += @amount
+      @status = "reversed"
+    end
   end
 end
